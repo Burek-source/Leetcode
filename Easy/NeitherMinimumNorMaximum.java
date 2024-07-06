d7bfefe269d2862ef65e76200e453efd2f990b6d
Burek-source <153553586+Burek-source@users.noreply.github.com>
2024-07-06 09:50:07 +0100

    Create NeitherMinimumNorMaximum.java
    Problem #2733

---
+++ Easy/NeitherMinimumNorMaximum.java
@@ -0,0 +1,62 @@
+Given an integer array nums containing distinct positive integers, find and return any number from the array that is neither the minimum nor the maximum value in the array, or -1 if there is no such number.
+
+Return the selected integer.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1,4]
+Output: 2
+Explanation: In this example, the minimum value is 1 and the maximum value is 4. Therefore, either 2 or 3 can be valid answers.
+
+  
+Example 2:
+
+Input: nums = [1,2]
+Output: -1
+Explanation: Since there is no number in nums that is neither the maximum nor the minimum, we cannot select a number that satisfies the given condition. Therefore, there is no answer.
+
+  
+Example 3:
+
+Input: nums = [2,1,3]
+Output: 2
+Explanation: Since 2 is neither the maximum nor the minimum value in nums, it is the only valid answer. 
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+All values in nums are distinct
+
+import java.util.Arrays;
+
+class Solution 
+{
+    public int findNonMinOrMax(int[] nums) 
+    {
+
+        Arrays.sort(nums);
+        int notMinMax = -1;
+
+        if(nums.length < 3)
+        {
+            return notMinMax;
+        }
+
+        // Works for cases where array consists of repeating elements
+        for(int i = 1; i < nums.length-1; i++)
+        {
+            if(nums[i] > nums[0] && nums[i] < nums[nums.length-1])
+            {
+                notMinMax = nums[i];
+                break;
+            }
+        }
+
+        return notMinMax;
+
+    }
+}
